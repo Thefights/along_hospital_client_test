e812948f0c650341e6759a9d48c0532e8f808593
DuyLP <leduy1159@gmail.com>
2025-10-19 14:40:44 +0700

    Implement CreateComplaintDialog with complaint topic and content fields; add complaintTopicOptions to useEnum

--- src/components/basePages/sections/medicalHistoryDetailSections/dialogs/CreateComplaintDialog.jsx
+++ src/components/basePages/sections/medicalHistoryDetailSections/dialogs/CreateComplaintDialog.jsx
@@ -1,5 +1,32 @@
-const CreateComplaintDialog = ({ open, onClose }) => {
-	return <div></div>
+import GenericFormDialog from '@/components/dialogs/commons/GenericFormDialog'
+import useEnum from '@/hooks/useEnum'
+import useTranslation from '@/hooks/useTranslation'
+
+const CreateComplaintDialog = ({ open, onClose, onSubmit = () => {} }) => {
+	const { t } = useTranslation()
+	const { complaintTopicOptions } = useEnum()
+
+	const fields = [
+		{
+			key: 'complaintTopic',
+			title: 'Complaint Topic',
+			type: 'select',
+			options: complaintTopicOptions,
+		},
+		{ key: 'content', title: 'Complaint Content', type: 'textArea', multiple: 3 },
+	]
+
+	return (
+		<GenericFormDialog
+			open={open}
+			onClose={onClose}
+			fields={fields}
+			title='Create Complaint'
+			submitLabel={t('button.create')}
+			submitButtonColor='primary'
+			onSubmit={onSubmit}
+		/>
+	)
 }
 
 export default CreateComplaintDialog
--- src/hooks/useEnum.js
+++ src/hooks/useEnum.js
@@ -16,6 +16,14 @@ export default function useEnum() {
 			{ value: 'Moderate', label: t('enum.severity_level.moderate') },
 			{ value: 'Severe', label: t('enum.severity_level.severe') },
 		],
+
+		complaintTopicOptions: [
+			{ value: 'Service', label: t('enum.complaint_topic.service') },
+			{ value: 'Billing', label: t('enum.complaint_topic.billing') },
+			{ value: 'Doctor', label: t('enum.complaint_topic.doctor') },
+			{ value: 'Medicine', label: t('enum.complaint_topic.medicine') },
+			{ value: 'Others', label: t('enum.complaint_topic.others') },
+		],
 	}
 }
 
