a686ac9c83c67d58fa7c4f68343272b6c99494fd
DuyLP <leduy1159@gmail.com>
2025-10-25 13:19:27 +0700

    Refactor GenericDrawer to use DrawerInfoRow for improved readability and maintainability; add SkeletonPatientCard component for consistent loading states.

--- src/components/skeletons/SkeletonBox.jsx
+++ src/components/skeletons/SkeletonBox.jsx
@@ -1,8 +1,20 @@
 import { Skeleton, Stack } from '@mui/material'
 
-const SkeletonBox = ({ numberOfBoxes = 3, heights = [100, 200, 300], rounded = false }) => {
+/**
+ * @param {Object} props
+ * @param {number} props.numberOfBoxes
+ * @param {'column' | 'row'} props.direction
+ * @param {number[]} props.heights
+ * @param {boolean} props.rounded
+ */
+const SkeletonBox = ({
+	numberOfBoxes = 3,
+	direction = 'column',
+	heights = [100, 200, 300],
+	rounded = false,
+}) => {
 	return (
-		<Stack spacing={2}>
+		<Stack spacing={2} direction={direction}>
 			{Array.from({ length: numberOfBoxes }).map((_, index) => (
 				<Skeleton
 					key={index}
