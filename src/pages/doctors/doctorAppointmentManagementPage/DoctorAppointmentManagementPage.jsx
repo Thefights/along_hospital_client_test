f4445fe79799d9c1b94ba56166fd360ef03cba47
DuyLP <leduy1159@gmail.com>
2025-10-29 02:23:56 +0700

    Update DoctorAppointmentManagementPage to utilize new success handlers for appointment actions

--- src/pages/doctors/doctorAppointmentManagementPage/DoctorAppointmentManagementPage.jsx
+++ src/pages/doctors/doctorAppointmentManagementPage/DoctorAppointmentManagementPage.jsx
@@ -2,6 +2,7 @@ import ManageAppointmentBasePage from '@/components/basePages/manageAppointmentB
 import ConfirmationButton from '@/components/generals/ConfirmationButton'
 import { ApiUrls } from '@/configs/apiUrls'
 import { EnumConfig } from '@/configs/enumConfig'
+import { routeUrls } from '@/configs/routeUrls'
 import { useAxiosSubmit } from '@/hooks/useAxiosSubmit'
 import useFetch from '@/hooks/useFetch'
 import useReduxStore from '@/hooks/useReduxStore'
@@ -9,6 +10,7 @@ import useTranslation from '@/hooks/useTranslation'
 import { setSpecialtiesStore } from '@/redux/reducers/managementReducer'
 import { Stack } from '@mui/material'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const DoctorAppointmentManagementPage = () => {
 	const [selectedAppointment, setSelectedAppointment] = useState(null)
@@ -18,6 +20,7 @@ const DoctorAppointmentManagementPage = () => {
 	})
 
 	const { t } = useTranslation()
+	const navigate = useNavigate()
 
 	const getAppointments = useFetch(
 		ApiUrls.APPOINTMENT.MANAGEMENT.GET_ALL_BY_CURRENT_DOCTOR,
@@ -33,14 +36,28 @@ const DoctorAppointmentManagementPage = () => {
 	const confirmAppointment = useAxiosSubmit({
 		url: ApiUrls.APPOINTMENT.MANAGEMENT.CONFIRM(selectedAppointment?.id),
 		method: 'PUT',
+		onSuccess: async () => {
+			await getAppointments.fetch()
+			setSelectedAppointment(null)
+		},
 	})
 	const completeAppointment = useAxiosSubmit({
 		url: ApiUrls.APPOINTMENT.MANAGEMENT.COMPLETE(selectedAppointment?.id),
 		method: 'PUT',
+		onSuccess: async (response) => {
+			const medicalHistoryId = response?.data?.medicalHistoryId
+			if (medicalHistoryId) {
+				navigate(routeUrls.BASE_ROUTE.DOCTOR(routeUrls.DOCTOR.MEDICAL_HISTORY.DETAIL(medicalHistoryId)))
+			}
+		},
 	})
 	const denyAssignment = useAxiosSubmit({
 		url: ApiUrls.APPOINTMENT.MANAGEMENT.DENY_ASSIGNMENT(selectedAppointment?.id),
 		method: 'PUT',
+		onSuccess: async () => {
+			await getAppointments.fetch()
+			setSelectedAppointment(null)
+		},
 	})
 
 	return (
@@ -63,6 +80,7 @@ const DoctorAppointmentManagementPage = () => {
 							confirmButtonColor='primary'
 							confirmButtonText={t('appointment.button.accept_assignment')}
 							loading={confirmAppointment.loading}
+							onConfirm={async () => await confirmAppointment.submit()}
 						>
 							{t('appointment.button.accept_assignment')}
 						</ConfirmationButton>
@@ -73,6 +91,7 @@ const DoctorAppointmentManagementPage = () => {
 							confirmButtonText={t('appointment.button.deny_assignment')}
 							color='error'
 							loading={denyAssignment.loading}
+							onConfirm={async () => await denyAssignment.submit()}
 						>
 							{t('appointment.button.deny_assignment')}
 						</ConfirmationButton>
@@ -85,6 +104,7 @@ const DoctorAppointmentManagementPage = () => {
 						confirmButtonText={t('appointment.button.complete_appointment')}
 						color='success'
 						loading={completeAppointment.loading}
+						onConfirm={async () => await completeAppointment.submit()}
 					>
 						{t('appointment.button.complete_appointment')}
 					</ConfirmationButton>
